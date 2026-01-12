ab6cc43acba9ab5f8e45142ded267fa278b3dd33
bananazon <cursed.bananazon@gmail.com>
2026-01-12 13:31:21 -0800

    Add 0.9.3

--- pstree@0.9.3.rb
+++ pstree@0.9.3.rb
@@ -1,9 +1,9 @@
-class PstreeAT092 < Formula
+class PstreeAT093 < Formula
   desc "Display a tree of processes."
   homepage "https://github.com/bananazon/pstree"
-  url "https://codeload.github.com/bananazon/pstree/tar.gz/refs/tags/v0.9.2"
-  sha256 "90c1e930094b689b9f0d1feb2d7ca6ffc0a9a54edadbe2eb397cae26c931ea28"
-  version "0.9.2"
+  url "https://codeload.github.com/bananazon/pstree/tar.gz/refs/tags/v0.9.3"
+  sha256 "4c0977e408bc35739f0e3e4631eb33d59974d2e2f54ca05ea417f2f00f52f7b0"
+  version "0.9.3"
 
   depends_on "go" => "1.23.5"
 
